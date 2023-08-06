a753498534bb68e61933aa01a87509afe044e635
Bahareh Jafary <baharehjafary1998@gmail.com~>
2023-08-06 16:45:32 +0330

    build landing page and set the route

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from "../components/molecules/NavBar";
+
+export const LandingPage = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LandingPage } from "../pages/LandingPage";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
