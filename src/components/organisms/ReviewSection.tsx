b657add2e984f68f8e510cb0e2a2bd93ec23ab16
Bahareh Jafary <baharehjafary1998@gmail.com~>
2023-08-19 15:35:21 +0330

    create review component base

--- src/components/organisms/ReviewSection.tsx
+++ src/components/organisms/ReviewSection.tsx
@@ -1,16 +1,35 @@
 import { Box, Container, Divider, Stack, Typography } from "@mui/material";
+import { DotShape } from "../atoms/DotShape";
 
 export const ReviewSection = () => {
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="lg">
       <Box
         sx={{
           background:
             "linear-gradient(208.18deg, #67C3F3 9.05%, #5A98F2 76.74%)",
           borderRadius: "24px",
+          textAlign: "center",
+          p: "100px",
+          position: "relative",
         }}
       >
-        <Typography>What our customer are saying</Typography>
+        <Box sx={{ position: "absolute", right: -270, top: 15 }}>
+          <DotShape numRows={4} numCols={3} />
+        </Box>
+        <Box sx={{ position: "absolute", left: -80, bottom: 15 }}>
+          <DotShape numRows={5} numCols={2} />
+        </Box>
+        <Typography
+          sx={{
+            fontSize: 36,
+            fontFamily: "Mulish",
+            fontWeight: 700,
+            color: "white",
+          }}
+        >
+          What our customer are saying
+        </Typography>
         <Divider
           sx={{
             background: "white",
@@ -20,19 +39,58 @@ export const ReviewSection = () => {
             my: 4,
           }}
         />
-        <Stack>
-          <Typography>
+        <Stack
+          direction="row"
+          justifyContent="space-around"
+          width="100%"
+          alignItems="center"
+          mt={10}
+        >
+          <Stack direction="row" alignItems="center" width="40%" spacing={4}>
+            <Box>
+              <img
+                src="/images/avatar.png"
+                alt="avatar"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </Box>
+            <Stack textAlign="left" spacing={1}>
+              <Typography
+                sx={{
+                  fontSize: 22,
+                  fontFamily: "Mulish",
+                  fontWeight: 700,
+                  color: "white",
+                }}
+              >
+                Edward Newgate
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: 18,
+                  fontFamily: "Mulish",
+                  fontWeight: 400,
+                  color: "white",
+                }}
+              >
+                Founder Circle
+              </Typography>
+            </Stack>
+          </Stack>
+          <Typography
+            sx={{
+              fontSize: 19,
+              fontFamily: "Mulish",
+              fontWeight: 400,
+              color: "white",
+              textAlign: "left",
+              width: "45%",
+            }}
+          >
             “Our dedicated patient engagement app and web portal allow you to
             access information instantaneously (no tedeous form, long calls, or
             administrative hassle) and securely”
           </Typography>
-          <Stack>
-            <Box></Box>
-            <Stack>
-              <Typography>Edward Newgate</Typography>
-              <Typography>Founder Circle</Typography>
-            </Stack>
-          </Stack>
         </Stack>
       </Box>
     </Container>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -19,6 +19,7 @@ export const LandingPage = () => {
       <DownloadApp />
       <Box sx={{ height: 300 }} />
       <ReviewSection />
+      <Box sx={{ height: 300 }} />
     </>
   );
 };
