f83f6d4e5d988c3c891f4fab92b00dc470312a14
Bahareh Jafary <baharehjafary1998@gmail.com~>
2023-08-19 14:12:52 +0330

    update landing, adding compos

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,19 @@
+import { Box } from "@mui/material";
 import { NavBar } from "../components/molecules/NavBar";
 import { HomePage } from "../components/organisms/HomePage";
+import { OurServices } from "../components/organisms/OurServices";
+import { OurPurpose } from "../components/organisms/OurPurpose";
 
 export const LandingPage = () => {
   return (
     <>
       <NavBar />
       <HomePage />
+      <Box sx={{ height: 150 }} />
+      <OurServices />
+      <Box sx={{ height: 150 }} />
+      <OurPurpose />
+      <Box sx={{ height: 150 }} />
     </>
   );
 };
