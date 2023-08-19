172c614a8d79300d4eeb3a9b83c2dc38a8f01988
Bahareh Jafary <baharehjafary1998@gmail.com~>
2023-08-19 11:16:57 +0330

    move dot shape function to atoms

---
+++ src/components/atoms/DotShape.tsx
@@ -0,0 +1,43 @@
+import { Box, Grid } from "@mui/material";
+import { FC } from "react";
+
+export const DotShape: FC<{ numRows: number; numCols: number }> = ({
+  numRows,
+  numCols,
+}) => {
+  const generateBoxes = () => {
+    const boxes = [];
+    for (let row = 0; row < numRows; row++) {
+      const rowBoxes = [];
+      for (let col = 0; col < numCols; col++) {
+        rowBoxes.push(
+          <Grid item key={row * numCols + col}>
+            <Box
+              sx={{
+                width: 12,
+                height: 12,
+                borderRadius: "50%",
+                backgroundColor: "#DFEDFD",
+              }}
+            />
+          </Grid>
+        );
+      }
+      boxes.push(
+        <Grid container spacing={2.5} key={row}>
+          {rowBoxes}
+        </Grid>
+      );
+    }
+    return boxes;
+  };
+
+  return (
+    <Box sx={{ px: 2 }}>
+      <Box sx={{ height: 200 }} />
+      <Grid container gap={2}>
+        {generateBoxes()}
+      </Grid>
+    </Box>
+  );
+};
