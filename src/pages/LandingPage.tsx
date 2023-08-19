559135b96cc2101fd73886bf38a287b73a1d987d
Bahareh Jafary <baharehjafary1998@gmail.com~>
2023-08-19 11:17:48 +0330

    update landing

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import { NavBar } from "../components/molecules/NavBar";
+import { HomePage } from "../components/organisms/HomePage";
 
 export const LandingPage = () => {
   return (
     <>
       <NavBar />
+      <HomePage />
     </>
   );
 };
